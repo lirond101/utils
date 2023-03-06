5048a14494fa89075ee73ac697db96b48459c615
lirond101 <lirond101@gmail.com>
2023-03-06 15:24:03 +0200

    Update start-docker-per-boot.sh

--- start-docker-per-boot.sh
+++ start-docker-per-boot.sh
@@ -1,1 +1,3 @@
+sudo touch /var/lib/cloud/scripts/per-boot/start_docker_per_boot.sh
+sudo chmod 777 /var/lib/cloud/scripts/per-boot/start_docker_per_boot.sh
 sudo echo "sudo service docker start" >> /var/lib/cloud/scripts/per-boot/start_docker_per_boot.sh
